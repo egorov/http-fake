008ee4d984f86dab66fdd7f2e686ec8d0800a714
Sergey Egorov <s.e.888@live.ru>
2018-08-28 20:17:41 +0500

    ClientRequestFake write and end stub methods have been added.

--- src/ClientRequestFake.js
+++ src/ClientRequestFake.js
@@ -2,6 +2,13 @@ const EventEmitter = require('events');
 
 class ClientRequestFake extends EventEmitter {
 
+    end(){
+
+    }
+    
+    write(data){
+
+    }
 }
 
 module.exports = ClientRequestFake;
--- tests/ClientRequestFake_tests.js
+++ tests/ClientRequestFake_tests.js
@@ -1,18 +1,32 @@
 describe('ClientRequestFake', () => {
 
     const ClientRequestFake = require('../src/ClientRequestFake');
+    let request = null;
+
+    beforeEach(() => {
+        request = new ClientRequestFake();
+    });
 
     it('should emit an error', () => {
 
-        const request = new ClientRequestFake();
         const errors = [];
         request.on('error', (error) => {
             errors.push(error);
-        })
+        });
 
         request.emit('error', 'Something goes wrong!');
 
         expect(errors.length).toEqual(1);
         expect(errors[0]).toEqual('Something goes wrong!');
+    });
+
+    it('should implement end method', () => {
+
+        expect(typeof request.end).toEqual('function');
+    });
+
+    it('should implement write method', () => {
+
+        expect(typeof request.write).toEqual('function');
     });
 });
